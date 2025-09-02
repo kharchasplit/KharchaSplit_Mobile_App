85d4f5631c9bba9afd687fdfe3337295c6017f41
Atharv Prasad <prasadatharv0702@gmail.com>
2025-09-02 18:53:53 +0530

    Add create new group

---
+++ src/screens/CreateNewGroupScreen.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CreateNewGroupScreen() {
+  return (
+    <View>
+      <Text>CreateNewGroupScreen</Text>
+    </View>
+  )
+}
